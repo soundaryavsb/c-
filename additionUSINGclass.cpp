f434d56a78c3807ed02eb72c1f393cfad790b41e
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-03-10 16:59:06 +0530

    Create additionUSINGclass.cpp

---
+++ additionUSINGclass.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class add
+{
+	int a,b;
+	public:
+	void showadd()
+	 {
+	 	a=10,b=15;
+	 	cout<<a+b;
+	 }
+};
+int main() {
+add a;
+a.showadd();
+	return 0;
+}
