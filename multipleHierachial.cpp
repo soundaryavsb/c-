ed121b4e1aa674baaac29420035b6d2bf6cba4d1
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-03-18 14:02:16 +0530

    Create multipleHierachial.cpp

---
+++ multipleHierachial.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+class grandfather
+{
+	public:
+	grandfather()
+	{
+		cout<<"This is grandfather"<<endl;
+	}
+};
+class grandmother
+{
+	public:
+	grandmother()
+	{
+		cout<<"this is grandmother"<<endl;
+	}
+};
+class dad:public grandfather,grandmother
+{
+	public:
+	dad()
+	{
+			cout<<"this is dad"<<endl;
+	}
+};
+class child1:public dad
+{
+	public:
+	child1()
+	{
+		cout<<"this is child1"<<endl;
+	}
+};
+class child2:public dad
+{
+	public:
+	child2()
+	{
+			cout<<"this is child2"<<endl;
+	}
+};
+int main() {
+	child1 ob;
+	child2 obj;
+	return 0;
+}
+ 
