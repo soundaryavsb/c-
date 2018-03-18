cc46fa61d5682a702975eea140964752623e31f7
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-03-18 12:07:18 +0530

    Create HierarchicalInheritance.cpp

---
+++ HierarchicalInheritance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class arya
+{
+	public:
+	arya()
+	{
+		cout<<"This is arya"<<endl;
+	}
+};
+class arya1:public arya
+{
+	public:
+	arya1()
+	{
+		cout<<"this is arya1"<<endl;
+	}
+};
+class arya2:public arya
+{
+	public:
+	arya2()
+	{
+			cout<<"this is arya2"<<endl;
+	}
+};
+int main() {
+	arya1 ob;
+	arya2 obj;
+	return 0;
+}
