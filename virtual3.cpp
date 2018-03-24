ec390f0b92ecc397d7d8454c10d3e7b427c3e27e
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-03-24 12:19:28 +0530

    Create virtual3.cpp

---
+++ virtual3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class bajaj
+{
+	public:
+	virtual void run()
+	{
+		cout<<"Default"<<endl;
+	}
+};
+class pulsar:public bajaj
+{
+	public:
+/*	void run()
+	{
+		cout<<"pulsar running "<<endl;
+	}*/
+};
+class discover:public bajaj
+{
+	public:
+/*	void run()
+	{
+		cout<<"discover is running "<<endl;
+	}*/
+};
+
+int main() {
+pulsar p;
+discover d;
+/*bajaj *b1=&p;
+bajaj *b2=&d;*/
+p.run();
+d.run();
+	return 0;
+}
