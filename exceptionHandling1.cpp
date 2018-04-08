bc302236533a9aca4460431df30621a646db6707
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-04-08 12:02:46 +0530

    Create exceptionHandling1.cpp

---
+++ exceptionHandling1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<"Basic program on exception";
+	try
+	{
+		int momage=45;
+		int sonage=15;
+		if( momage<sonage)//not true .so not throw the exception
+		{
+			throw 404;
+		}
+	}catch(int x)
+	{
+		cout<<"\n mom can't be younger than son\nERROR CODE:"<<x;
+	}
+	return 0;
+}
